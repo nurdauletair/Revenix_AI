fa8bcbe6546c81d183dfcc3cc76a4c725e367280
Nurdaulet <nurdauletmac@gmail.com>
2026-04-27 14:23:48 +0500

    add express server

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 require("dotenv").config();
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// тестовый роут
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+// запуск Telegram
 require("./channels/telegram");
 
-console.log("🚀 App started");
+// запуск сервера
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
