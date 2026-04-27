5f62ae3d8cabee15a7c9d60a4bb80091e0dd4117
Nurdaulet <nurdauletmac@gmail.com>
2026-04-27 15:23:24 +0500

    add instagram webhook

--- app.js
+++ app.js
@@ -12,8 +12,28 @@ app.get("/", (req, res) => {
 
 require("./channels/telegram");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.META_VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("📩 Instagram event:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
 });
